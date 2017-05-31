3701079b567c5e34646bf89ef74162dc89e88769
Angela Gomez <anggomez1@cobogpgp1290.global.publicisgroupe.net>
2017-05-31 17:14:33 -0500

    Add RegexEmail

--- src/main/java/com/contract/constant/RegexConstant.java
+++ src/main/java/com/contract/constant/RegexConstant.java
@@ -1,20 +1,10 @@
 package com.contract.constant;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Created by anggomez1 on 5/31/17.
  */
 public class RegexConstant {
 
-    private Pattern pattern;
-    private Matcher matcher;
-
-    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
-    public RegexConstant() {
-        pattern = Pattern.compile(EMAIL_PATTERN);
-    }
+    public static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
 }
---
+++ src/main/java/com/contract/util/ValidateEmail.java
@@ -0,0 +1,20 @@
+package com.contract.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.contract.constant.RegexConstant.*;
+
+/**
+ * Created by anggomez1 on 5/31/17.
+ */
+public class ValidateEmail {
+
+    private Pattern pattern;
+    private Matcher matcher;
+
+
+    public ValidateEmail() {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+    }
+}
